38736da147d256724249dc8110ef47e7f6c97a55
Ayaan Pathan <158172060+Ayaanpathan7@users.noreply.github.com>
2026-04-04 23:22:05 +0530

    Add solution for Assign Cookies problem
    Implemented a solution to find the maximum number of content children by sorting the greed factor and cookie sizes, and using a two-pointer approach.

--- Greedy-Problems/AssignCookies.java
+++ Greedy-Problems/AssignCookies.java
@@ -1,1 +1,55 @@
-public class AssignCookies { } 
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        if(s.length==0 || g.length==0) return 0;
+        g = sortArray(g);
+        s = sortArray(s);
+        int count = 0,i=0,j=0;
+        while(j<s.length && i<g.length){
+            if(g[i]<=s[j]){
+                count++;
+                i++;j++;
+            }
+            else j++;
+
+        } 
+        return count;
+    }
+    public int[] sortArray(int[] nums) {
+        if(nums.length<=1) return nums;
+        int mid = (nums.length/2);
+        int[] left = sortArray(Arrays.copyOfRange(nums, 0 , mid));
+        int[] right = sortArray(Arrays.copyOfRange(nums, mid, nums.length));
+        return mergeSort(left,right);
+    }
+    public int[] mergeSort(int[] first, int[] second){
+        int[] newArr = new int[first.length+second.length];
+        int i=0;
+        int j =0;
+        int k=0;               
+        while(i<first.length && j<second.length){
+
+            if(first[i]>second[j]){
+                newArr[k] = second[j];
+                j++;
+            }
+            else{
+                newArr[k]= first[i];
+                i++;
+            }
+            k++;
+        }
+        while(i<first.length){
+            newArr[k] = first[i];
+            i++;
+            k++;
+        }
+        while(j<second.length){
+            newArr[k] = second[j];
+            j++;
+            k++;
+        }
+        return newArr;
+
+    }
+
+}
