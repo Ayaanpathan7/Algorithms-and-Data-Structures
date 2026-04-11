64f394671bd15029ca8516bdbbd1b8069949a542
Ayaan Pathan <158172060+Ayaanpathan7@users.noreply.github.com>
2026-04-11 23:50:21 +0530

    Add cycle detection method in LinkedListCycle2
    Implement Floyd's Tortoise and Hare algorithm to detect a cycle in a linked list.

---
+++ LinkedList/LinkedListCycle2.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                
+                ListNode temp = head;
+                while (temp != slow) {
+                    temp = temp.next;
+                    slow = slow.next;
+                }
+                return temp; 
+            }
+        }
+        return null; 
+    }
+}
